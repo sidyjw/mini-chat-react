65f1ceaf87ee5d2a4791b01c30e82b8e1d4bb202
Sidiney Silva <sidineysilva1@gmail.com>
2020-10-06 10:13:22 -0300

    Finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Widget, addResponseMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
+import io from "socket.io-client";
 
 function App() {
-  return <p>Olá mundo</p>;
+  const socketRef = React.useRef();
+  socketRef.current = io.connect("http://localhost:5500");
+
+  const sendMessage = (msg) => socketRef.current.emit("new-message", msg);
+
+  React.useEffect(() => {
+    socketRef.current.on("connect", () => {
+      socketRef.current.on("add-new-message", (msg) => addResponseMessage(msg));
+    });
+  });
+  return <Widget title="Chat" subtitle="" handleNewUserMessage={sendMessage} />;
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5500;
+const socketio = require("socket.io");
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`[Server] Running on: http://localhost:${PORT} `)
 );
+
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("new-message", (msg) =>
+    socket.broadcast.emit("add-new-message", msg)
+  );
+});
